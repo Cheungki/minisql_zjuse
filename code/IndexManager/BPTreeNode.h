7401d42b67b2dc421198e02596ddfe0a1599f0fe
thebigdoudou <15888690596@163.com>
2020-06-27 22:05:45 +0800

    6.27 更新了BPTreeNode

--- code/IndexManager/BPTreeNode.h
+++ code/IndexManager/BPTreeNode.h
@@ -18,6 +18,21 @@ public:
 
     ~BPTreeNode();
 
+    int getSize() const;
+    int getKeyLength() const;
+    bool isLeaf() const;
+    const char* getKey(int pos) const;
+    int getPtr(int pos) const;
+    int findPos(const char* key);
+    void placeKey(int pos, const char* key);
+    void placePtr(int pos, int ptr);
+    void setRemoved();
+    void insert(int pos, const char* key, int ptr);
+    void remove(int pos);
+    BPTreeNode* split(int newId, char* newKey);
+    const char* borrowFromSib(BPTreeNode* sib, bool isLeft, const char* parentKey);
+    void mergeRight(BPTreeNode* sib, const char* parentKey);
+
 private:
     string filename;
     int id;
@@ -29,6 +44,28 @@ private:
     bool removed;
     vector<char*> keys;
     vector<int> ptrs;
+    int nodecmp(const char* a,const  char* b)
+    int nodecmp(const char* a,const  char* b)
+    {
+        switch(dataType){
+            case miniSQL_INT:
+                int ta, tb;
+                fileManager::readInt(a, &ta);
+                fileManager::readInt(b, &tb);
+                return ta - tb;
+            case miniSQL_FLOAT:
+                float fa, fb;
+                fileManager::readFloat(a, &fa);
+                fileManager::readFloat(b, &fb);
+                if (fa - fb < 0) return -1;
+                else if (fa - fb > 0) return 1;
+                else return 0;
+            case miniSQL_CHAR:
+                return memcmp(a, b, keyLength);
+        }
+        cerr << "ERROR: DataType Error, Not any of DB_INT/FLOAT/CHAR" << endl;
+        return 0;
+    }
 };
 
 
